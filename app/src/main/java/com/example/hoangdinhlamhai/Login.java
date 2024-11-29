8d55937a5ae9ab76586df4dba54d5d624ed797cd
hoangdinhlamhai <hoangdinhlamhai1@gmail.com>
2024-11-29 08:20:00 +0700

    finish

---
+++ app/src/main/java/com/example/hoangdinhlamhai/Login.java
@@ -0,0 +1,34 @@
+package com.example.hoangdinhlamhai;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Login extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout
+        return inflater.inflate(R.layout.activity_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button signInButton = view.findViewById(R.id.btnSignIn);
+
+        signInButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), HomePage.class);
+            startActivity(intent);
+        });
+    }
+}
--- app/src/main/java/com/example/hoangdinhlamhai/MainActivity.java
+++ app/src/main/java/com/example/hoangdinhlamhai/MainActivity.java
@@ -7,18 +7,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Set mặc định Fragment Home
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new Login())
+                .commit();
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            if (item.getItemId() == R.id.menu_home) {
+                selectedFragment = new Login();
+            } else if (item.getItemId() == R.id.menu_user) {
+                selectedFragment = new UserFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+            return true;
         });
+
     }
 }
